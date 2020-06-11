69b68ae7197c6e951fc7fa0a4f6f7f7df770ac23
Ari <ariannasg.ldn@gmail.com>
2020-06-11 13:39:11 +0100

    Added comments regarding the units of measurement that can be shown by timeit

---
+++ src/units_of_measurement.py
@@ -0,0 +1,16 @@
+#!usr/bin/env python3
+
+# Units of measurement that can be shown by timeit:
+# We have milliseconds, which are abbreviated with ms and we have 1,000 of
+# these in one second.
+#
+# After that we have microseconds which are abbreviated with mu (μ), the Greek
+# letter μS, and there are 1,000,000 of these in a second.
+#
+# And then we have nanoseconds, abbreviated with ns,
+# and there are 1,000,000,000 of these in a second.
+
+
+# 1sec -> 1 thousand ms (milliseconds)
+# 1sec -> 1 million μS (microseconds)
+# 1sec -> 1 billion ns (nanoseconds)
